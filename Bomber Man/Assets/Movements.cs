c9bd83e5d4e4f5906396e61fea0260a2ab92af43
adarshverma236 <adarshverma11236@gmail.com>
2020-02-01 01:53:58 +0200

    Bomber moving & stopped after hit

---
+++ Bomber Man/Assets/Movements.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+    /*private float Speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = 5;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.Getkey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-10 * Time.detaTime,0, 0);
+        }
+        if (Input.Getkey(KeyCode.RightArrow))
+        {
+            transform.Translate(10 * Time.detaTime,0, 0);
+        }
+
+    }*/
+}
---
+++ Bomber Man/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f;
+    
+    private float minX = -2.55f;
+    private float maxX = 2.55f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector2 currentPosition = transform.position;
+
+        if (h > 0)
+        {
+            // goind to yhe right
+
+            currentPosition.x += speed * Time.deltaTime;
+        }else if (h < 0)
+        {
+            // going to the left
+            
+            currentPosition.x -= speed * Time.deltaTime;
+        }
+
+        if (currentPosition.x < minX)
+        {
+            currentPosition.x = minX;
+        }
+        if (currentPosition.x > maxX)
+        {
+            currentPosition.x = maxX;
+        }
+
+        transform.position = currentPosition;
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.CompareTag("Bomb"))
+        {
+            Time.timeScale = 0f;
+        }
+
+    }
+}
